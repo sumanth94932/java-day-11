39f9586540bb329f2917188cfb4c08d80f0e531a
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-12 12:22:22 +0530

    Add files via upload

---
+++ markerinter.java
@@ -0,0 +1,14 @@
+import java.util.*;
+interface printable{
+    }
+class h implements printable{
+    public void print(){
+    System.out.println();
+    }
+}
+public class Main{
+    public static void main(String args[]){
+        h f=new h();
+        f.print();
+}
+}
